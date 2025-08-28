c9fb82ae584c0f8319d7006aba6a9a47004668a2
coconhat <nhat_vu_le@dlsl.edu.ph>
2025-08-28 21:38:11 +0800

    fix: remove some type

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,23 +1,7 @@
-export interface Attendance {
-  studentId: string;
-  fullName: string;
-  department: string;
-  email: string;
-  timestamp: string;
-}
-
 export interface Student {
   email_address: string;
   department: string;
   photoUrl: string | null;
   fullName: string; // Derived from email or other source
   partner_id: string; // The "real" unique student identifier from the API
-}
-
-export interface Event {
-  id: string;
-  name: string;
-  date: string;
-  description: string;
-  attendees: Attendance[];
 }
